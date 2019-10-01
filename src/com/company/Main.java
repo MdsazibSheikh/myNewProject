5220b505b2b56cfa3fea20f808297ae85e5975c2
unknown <sazibsheikh95@gmail.com>
2019-10-01 19:12:53 +0600

    add file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        arrayDesign();
         System.out.println("hellow world");
     }
+
+    static int indexOfArray = 10;
+
+    static void arrayDesign () {
+
+        System.out.println("----------");
+        for (int i = 0; i < indexOfArray; i++) {
+
+            int randomVal = (int) (Math.random() *5) + 10;
+
+              System.out.println(" | "+i+ " | "+ randomVal + " | ");
+        }
+        System.out.println("----------");
+    }
 }
+
+
+
+
